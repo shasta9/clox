560a4d320c59d72c7000fe29e100f6825d4ec4c2
John Hutchinson <john.hutchinson@pcs-instruments.com>
2018-07-12 19:50:27 +0100

    Chapter 15 - A Virtual Machine

--- main.c
+++ main.c
@@ -3,19 +3,35 @@
 #include "debug.h"
 #include "vm.h"
 
+InterpretResult interpret(Chunk* chunk);
+
 int main(int argc, const char* argv[]) {
     initVM();
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.234);
+    int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
-    writeChunk(&chunk, OP_RETURN, 124);
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
     return 0;
 }
+
--- vm.h
+++ vm.h
@@ -3,11 +3,25 @@
 
 #include "chunk.h"
 
+#define STACK_MAX 256
+
 typedef struct {
     Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
 } VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
 
 #endif //CLOX_VM_H
