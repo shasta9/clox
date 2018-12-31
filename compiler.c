9d781ec1282450f679f7853d239d6804c635dba7
John Hutchinson <john.hutchinson@pcs-instruments.com>
2018-12-31 17:54:52 +0000

    WIP - Chapter 17.4 Parsing Prefix Expressions

--- compiler.c
+++ compiler.c
@@ -12,15 +12,36 @@ typedef struct {
     bool panicMode;
 } Parser;
 
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! - +
+    PREC_CALL,        // . () []
+    PREC_PRIMARY
+} Precedence;
+
 Parser parser;
 Chunk* compilingChunk;
 
 static void advance();
+static void expression();
 static void consume(TokenType type, const char* message);
+static void endCompiler();
+static void grouping();
+static void number();
+static void unary();
+static void parsePrecedence(Precedence precedence);
 static void emitBytes(uint8_t byte1, uint8_t byte2);
 static void emitByte(uint8_t byte);
-static void endCompiler();
 static void emitReturn();
+static void emitConstant(Value value);
+static uint8_t makeConstant(Value value);
 static Chunk* currentChunk();
 static void errorAtCurrent(const char* message);
 static void error(const char* message);
@@ -36,7 +57,7 @@ bool compile(const char* source, Chunk* chunk) {
     parser.panicMode = false;
 
     advance();
-   // expression();
+    expression();
     consume(TOKEN_EOF, "Expect end of expression.");
     endCompiler();
     return !parser.hadError;
@@ -53,12 +74,46 @@ static void advance() {
     }
 }
 
+static void expression() {
+    parsePrecedence(PREC_ASSIGNMENT);
+}
+
 static void consume(TokenType type, const char* message) {
     if (parser.current.type == type) {
         advance();
         return;
     }
     errorAtCurrent(message);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number() {
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void unary() {
+    TokenType operatorType = parser.previous.type;
+    // compile the operand
+    parsePrecedence(PREC_UNARY);
+    // emit the operator instruction
+    switch (operatorType) {
+        case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+        default:
+            return; // unreachable
+    }
+}
+
+static void parsePrecedence(Precedence precedence) {
+    // What goes here?
 }
 
 static void emitBytes(uint8_t byte1, uint8_t byte2) {
@@ -70,12 +125,21 @@ static void emitByte(uint8_t byte) {
     writeChunk(currentChunk(), byte, parser.previous.line);
 }
 
-static void endCompiler() {
-    emitReturn();
-}
-
 static void emitReturn() {
     emitByte(OP_RETURN);
+}
+
+static void emitConstant(Value value) {
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static uint8_t makeConstant(Value value) {
+    int constant = addConstant(currentChunk(), value);
+    if (constant > UINT8_MAX) {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+    return (uint8_t)constant;
 }
 
 static Chunk* currentChunk() {
@@ -91,6 +155,7 @@ static void error(const char* message) {
 }
 
 static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
     parser.panicMode = true;
 
     fprintf(stderr, "[line %d] Error", token->line);
