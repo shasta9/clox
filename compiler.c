035edff0815dd9bd2549e54ace9a2e2ba44eacc3
John Hutchinson <john.hutchinson@pcs-instruments.com>
2018-12-31 13:25:46 +0000

    WIP - Chapter 17.2 Parsing Tokens

--- compiler.c
+++ compiler.c
@@ -1,12 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
 
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void advance();
+static void consume(TokenType type, const char* message);
+static void errorAtCurrent(const char* message);
+static void error(const char* message);
+static void errorAt(Token* token, const char* message);
+
+
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
     advance();
-    expression();
+   // expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
 
+static void advance() {
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+    errorAtCurrent(message);
+}
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.previous, message);
+}
+
+static void error(const char* message) {
+    errorAt(&parser.previous, message);
+}
+
+static void errorAt(Token* token, const char* message) {
+    parser.panicMode = true;
+
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // nothing
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
