aa438ac7c8e63de373f0d3674143554e2389cb89
John Hutchinson <john.hutchinson@pcs-instruments.com>
2018-12-31 13:07:59 +0000

    WIP - Chapter 17

--- compiler.c
+++ compiler.c
@@ -1,22 +1,12 @@
-#include <stdio.h>
-
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    initScanner(source);
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
 
-        if (token.type == TOKEN_EOF) break;
-    }
+bool compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
+
